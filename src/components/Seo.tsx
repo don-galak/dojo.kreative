6706848b6dd912f49afb4b6811863880fff96dd8
don-galak <panagiotis.tagkalakis@dialecticanet.com>
2023-07-04 09:51:17 +0300

    change seo related meta

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,12 +3,11 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+  title: 'dojo.kreative',
+  siteName: 'dojo.kreative',
+  description: 'A diruptive free-lance web design service',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://dojo-kreative.vercel.app/',
   type: 'website',
   robots: 'follow, index',
   /**
